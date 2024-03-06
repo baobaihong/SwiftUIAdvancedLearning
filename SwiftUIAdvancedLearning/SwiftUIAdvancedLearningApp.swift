66f8e2a5cd018f16c18d2c64f883f1e80e3f02f0
Jason Bao <baihongbao@outlook.com>
2024-03-06 18:00:29 +0800

    Initial Commit

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Jason on 2024/3/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
