944449d035bedeff7f829448a077869e5158d4f0
Jason Bao <baihongbao@outlook.com>
2024-03-07 17:25:26 +0800

    cusom arc and quad curve

---
+++ SwiftUIAdvancedLearning/CusomeCurvesBootcamp.swift
@@ -0,0 +1,94 @@
+//
+//  CusomeCurvesBootcamp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Jason on 2024/3/7.
+//
+
+import SwiftUI
+
+struct ArcSample: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 0), // <- start at the mid right
+                endAngle: Angle(degrees: 40),
+                clockwise: true)
+        }
+    }
+}
+
+struct ShapeWithArc: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            // top left
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            
+            // top right
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            
+            //mid right
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            
+            // bottom
+            // path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.width / 2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false)
+            
+            // mid left
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        }
+    }
+}
+
+struct QuadSample: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.maxX, y: rect.minY))
+        }
+    }
+}
+
+struct WaterShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.25, y: rect.height * 0.40))
+            path.addQuadCurve(
+                to: CGPoint(x: rect.maxX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.75, y: rect.height * 0.60))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
+
+struct CusomeCurvesBootcamp: View {
+    var body: some View {
+        WaterShape()
+            .fill(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.blue, Color.teal]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing)
+            )
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    CusomeCurvesBootcamp()
+}
+
